6a429a4a0f452a5bc134bb58430582b92138c4b6
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2021-12-25 20:19:32 +0000

    updated Loops.java

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -3,8 +3,19 @@ import java.util.*;
 
 public class Loops{
 
-  public static void main(String[]args){
+//Think Java Ch 7 Exercises
+  //Exercise 2 squareRoot
+  public static double squareRoot(double a){
+    double temp;
+    double sr = a/2;
+    do { temp = sr;
+    sr = (temp + a/temp)/2;
+    } while (Math.abs(sr-temp)>=0.0001);
+    return a;
+  }
 
+  public static void main(String[]args){
+    System.out.println(squareRoot(2));
     System.out.println("debug check OK");
   
   }
