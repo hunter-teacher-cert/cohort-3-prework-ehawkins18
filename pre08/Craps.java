53778b14c2cb37006fcfbe9978ca4f9ea22454df
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2021-12-26 15:41:22 +0000

    updated Craps.java

--- pre08/Craps.java
+++ pre08/Craps.java
@@ -6,20 +6,32 @@ public class Craps{
 //Method roll for rolling a die
   public static int roll(int maxRoll){
     //returns an integer between 1 and maxRoll (both includesive)
-  return maxRoll;
+    Random random = new Random();
+    int numRolled = random.nextInt(maxRoll) + 1;
+  return numRolled;
   }
 //Method shoot for rolling multiple dice
   public static int shoot(int numDice, int maxRoll){
     //returns an integer between numDice and (numDice * maxRoll) (both inclusive)
-  return numDice;
+    int rollTotal = 0;
+    for (int i = 1; i <= numDice; i++){
+      rollTotal += roll(maxRoll);
+    }
+  return rollTotal;
   }
 //Method round for determining if the shooter wins or loses
   public static boolean round(){
     //returns true if the shooter wins the round and returns false if the shooter loses the round
-  Boolean shooterWins = true;
-  return shooterWins;
+    Boolean shooterWins;
+    int shooterRoll = shoot(2,6);
+    System.out.println(shooterRoll);
+    if(shooterRoll == 2 || shooterRoll == 3 || shooterRoll == 12) {return false;}
+    if(shooterRoll == 7 || shooterRoll == 11) {return true;} else {shooterWins = round();}
+    
+    return shooterWins;
   }
   public static void main(String[]args){
+    System.out.println(round());
     System.out.println("OK");
   }
 }
