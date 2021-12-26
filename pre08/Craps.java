39eea46c38a53d819f1aca0668afbdd2b7c3055d
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2021-12-26 13:50:09 +0000

    created pre08 and started Craps.java

---
+++ pre08/Craps.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Craps{
+
+//Method roll for rolling a die
+  public static int roll(int maxRoll){
+    //returns an integer between 1 and maxRoll (both includesive)
+  return maxRoll;
+  }
+//Method shoot for rolling multiple dice
+  public static int shoot(int numDice, int maxRoll){
+    //returns an integer between numDice and (numDice * maxRoll) (both inclusive)
+  return numDice;
+  }
+//Method round for determining if the shooter wins or loses
+  public static boolean round(){
+    //returns true if the shooter wins the round and returns false if the shooter loses the round
+  Boolean shooterWins = true;
+  return shooterWins;
+  }
+  public static void main(String[]args){
+    System.out.println("OK");
+  }
+}
