10edd3e91625c082465ec31c49010c3cf9c7244c
Ed Hawkins <96295230+ehawkins18@users.noreply.github.com>
2021-12-27 21:35:35 +0000

    Updated Sieve.java

--- pre07/Sieve.java
+++ pre07/Sieve.java
@@ -3,10 +3,28 @@ import java.util.*;
 
 public class Sieve{
 
-public static void main(String[]args){
+  public static boolean[] sieve(int n){
+    //create array 
+    boolean[] a = new boolean [n];
+    for(int i = 0; i < a.length; i++){
+    a[i] = true;
+    }
+    //apply the sieve to the array
+    for(int i = 2; i < a.length; i++){
+      for(int j = 2; i*j < a.length; j++){
+        a[i*j] = false;
+      }
+    }
+  return a;
+  }
 
+  public static void main(String[]args){
+  //create array representing all positive integers up to the given number n
+  boolean[] isPrime = new boolean [20];
+  isPrime = sieve(20);
+  System.out.println(Arrays.toString(isPrime));
   System.out.println("Debug OK");
-}
+  }
 
 
 }
